0d24197e4ce3c2cf25b0be25542fe271e931c8a9
ZaidAhmed123-dev <zaidahmed141@gmail.com>
2025-02-03 21:03:46 +0500

    Update task1.cpp

--- LAB 2/task1.cpp
+++ LAB 2/task1.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int n;
+    cout << "Enter the size of the array\n";
+    cin >> n;
+
+    int *arr;
+
+    arr = new int[n];
+
+    cout << "enter the numbers of the array: \n";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int sum = 0, avg, max = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+
+    avg = sum / n;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    cout << "sum: " << sum << "\naverage: " << avg << "\nmaximum value: " << max;
+
+    delete[] arr;
+}
