7723b3127d8cdd7d224e9e1664b55593b1eb3e57
ZaidAhmed123-dev <zaidahmed141@gmail.com>
2025-04-18 23:55:07 +0500

    Create task 3.cpp

---
+++ LAB 9/task 3.cpp
@@ -0,0 +1,19 @@
+#include "Book.h"
+
+Book::Book(string t, string a, string i) {
+    title = t;
+    author = a;
+    ISBN = i;
+}
+
+string Book::getTitle() const {
+    return title;
+}
+
+string Book::getAuthor() const {
+    return author;
+}
+
+string Book::getISBN() const {
+    return ISBN;
+}
